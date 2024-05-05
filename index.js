e7f76fd6f3e8cb789258b11ffdaa38abe24a16fd
girishjain01 <gngit2010@gmail.com>
2024-05-05 13:24:44 +0530

    abc6

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const Project = require("./dbconn/connection");
-const Sroject = require("./dbconn/srojectconnection");
+const c = require("./dbconn/srojectconnection");
 const app = express();
 const cors = require('cors');
+const Sroject = require("./dbconn/srojectconnection");
 app.use(cors());
 app.use(express.json()); // For parsing application/json
 const port = 5000;
@@ -26,6 +27,7 @@ app.post("/sroject", async (req, res) => {
     }
 });
 
+
 app.put("/projects/:id", async (req, res) => {
     try {
         const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -39,10 +41,12 @@ app.put("/projects/:id", async (req, res) => {
 });
 app.put("/srojects/:id", async (req, res) => {
     try {
-        const Srojec = await Sroject.find({});
-        res.status(200).send(Srojec);
+        const updatedSroject = await Sroject.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedSroject) {
+            return res.status(404).send('Sroject not found');
+        }
+        res.send(updatedSroject);
     } catch (error) {
-        console.log(error.message);
         res.status(500).send(error);
     }
 });
@@ -57,7 +61,7 @@ app.get("/projects/:id", async (req, res) => {
         res.status(500).send(error);
     }
 });
-app.get("/sroject/:id", async (req, res) => {
+app.get("/sroject", async (req, res) => {
     try {
         const Srojec = await Sroject.find({});
         res.status(200).send(Srojec);
