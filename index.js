5e001c6dfe876537447a7c0e98a5a1a07ac95ab3
girishjain01 <gngit2010@gmail.com>
2024-05-10 13:52:17 +0530

    klm

--- index.js
+++ index.js
@@ -1,98 +1,60 @@
 const express = require("express");
-const Project = require("./dbconn/connection");
-const c = require("./dbconn/srojectconnection");
-const app = express();
 const cors = require('cors');
+require ('dotenv').config();
+const Project = require("./dbconn/connection");
 const Sroject = require("./dbconn/srojectconnection");
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json()); // For parsing application/json
-const port = 3000;
-// POST route to add a new project
-app.post("/", async (req, res) => {
-    const myProject = new Project(req.body);
+
+// Generic function to add a new document
+async function addDocument(Model, req, res) {
+    const document = new Model(req.body);
     try {
-        const dataSave = await myProject.save();
-        res.status(201).send(dataSave);
+        const savedDocument = await document.save();
+        res.status(201).send(savedDocument);
     } catch (error) {
         res.status(500).send(error);
     }
-});
-app.post("/sroject", async (req, res) => {
-    const mySroject = new Sroject(req.body);
+}
+
+// Generic function to update a document
+async function updateDocument(Model, req, res) {
     try {
-        const dataSave = await mySroject.save();
-        res.status(201).send(dataSave);
+        const updatedDocument = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!updatedDocument) return res.status(404).send('प्रोजेक्ट नहीं मिला');
+        res.send(updatedDocument);
     } catch (error) {
         res.status(500).send(error);
     }
-});
+}
 
-
-app.put("/project/:id", async (req, res) => {
+// Routes for Project
+app.post("/project", (req, res) => addDocument(Project, req, res));
+app.get("/project/:id", async (req, res) => {
     try {
-        const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!project) {
-            return res.status(404).send('प्रोजेक्ट नहीं मिला');
-        }
+        const project = await Project.findById(req.params.id);
+        if (!project) return res.status(404).send('प्रोजेक्ट नहीं मिला');
         res.send(project);
     } catch (error) {
         res.status(500).send(error);
     }
 });
-app.put("/sroject/:id", async (req, res) => {
+app.put("/project/:id", (req, res) => updateDocument(Project, req, res));
+
+// Routes for Sroject
+app.post("/sroject", (req, res) => addDocument(Sroject, req, res));
+app.get("/sroject/:id", async (req, res) => {
     try {
-        const sroject = await Sroject.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!sroject) {
-            return res.status(404).send('प्रोजेक्ट नहीं मिला');
-        }
+        const sroject = await Sroject.findById(req.params.id);
+        if (!sroject) return res.status(404).send('प्रोजेक्ट नहीं मिला');
         res.send(sroject);
     } catch (error) {
         res.status(500).send(error);
     }
 });
-app.get("/project/:id", async (req, res) => {
-    try {
-        const project = await Project.findById(req.params.id);
-        if (!project) {
-            return res.status(404).send('प्रोजेक्ट नहीं मिला');
-        }
-        res.send(project);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-app.get("/sroject/:id", async (req, res) => {
-    try {
-        const project = await Sroject.findById(req.params.id);
-        if (!project) {
-            return res.status(404).send('प्रोजेक्ट नहीं मिला');
-        }
-        res.send(project);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-app.get("/sroject", async (req, res) => {
-    try {
-        const Srojec = await Sroject.find({});
-        res.status(200).send(Srojec);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send(error);
-    }
-});
-// GET route to fetch all projects
-// Express सर्वर कोड
-app.get("/project", async (req, res) => {
-    try {
-        const projects = await Project.find({});
-        res.status(200).send(projects);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
-
-
+app.put("/sroject/:id", (req, res) => updateDocument(Sroject, req, res));
 
 // Start the server
 app.listen(port, () => {
