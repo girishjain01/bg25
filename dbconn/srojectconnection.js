41eaf44caf409d273619f9d5a2a942d259439b72
girishjain01 <gngit2010@gmail.com>
2024-05-24 14:51:12 +0530

    lll

--- dbconn/srojectconnection.js
+++ dbconn/srojectconnection.js
@@ -1,144 +1,270 @@
 const mongoose = require("mongoose");
-require ('dotenv').config();
+require('dotenv').config();
 
 mongoose.connect('mongodb+srv://gngit2010:gngjain1234@cluster0.dahu3zg.mongodb.net/6prilmahaveer')
   .then(() => {
-    console.log("our connection is successfully");
+    console.log("Connection to the database was successful");
   }).catch((e) => {
     console.log(e);
   });
- 
+
 const schema = new mongoose.Schema({
-    _id:{
-        type: String,
-        required: true
-    },
-    des1:{
-        type:String, 
-    },
-    des2:{
-        type:String,
-    },
-    des3:{
-        type:String,    
-    },
-    des4:{
-        type:String,
-      },
-      des5:{
-        type:String,
-    },
-    des6:{
-      type:String,
-    },
-    des7:{
-      type:String,
-    },
-    des8:{
-    type:String,
-    },
-    des9:{
-      type:String,
-      },
-      des10:{
-        type:String, 
-    },
-    des11:{
-      type:String,
-    },
-    des12:{
-      type:String,
-    },
-    des13:{
-    type:String,
-    },
-    des14:{
-    type:String,
-    },
-    des15:{
-    type:String,
-    },
-    des16:{
-    type:String,
-    },
-    des17:{
-    type:String,
-    },
-    des18:{
-    type:String,
-    },
-    des19:{
-      type:String, 
-    },
-    des20:{
-    type:String,
-    },
-    des21:{
-    type:String,
-    },
-    des22:{
-    type:String,
-    },
-    des23:{
-    type:String,
-    },
-    des24:{
-    type:String,
-    },
-    des25:{
-    type:String,
-    },
-    des26:{
-    type:String,
-    },
-    des27:{
-    type:String,
-    },
-    des28:{
-    type:String, 
-    },
-    des29:{
-    type:String,
-    },
-    des30:{
-    type:String,
-    },
-    des31:{
-    type:String,
-    },
-    des32:{
-    type:String,
-    },
-    des33:{
-    type:String,
-    },
-    des34:{
-    type:String,
-    },
-    des35:{
-    type:String,
-    },
-    des36:{
-    type:String,
-    },
-    des37:{
-      type:String,
-      },
-      des38:{
-      type:String,
-      },
-      des39:{
-      type:String,
-      },
-      des40:{
-      type:String,
-      },
-      des41:{
-      type:String,
-      },
-      des42:{
-      type:String,
-      },
+  _id: {
+    type: String,
+    required: true
+  },
+  des1: {
+    type: String,
+  },
+  audio1: {
+    type: String,
+  },
+  des2: {
+    type: String,
+  },
+  audio2: {
+    type: String,
+  },
+  des3: {
+    type: String,
+  },
+  audio3: {
+    type: String,
+  },
+  des4: {
+    type: String,
+  },
+  audio4: {
+    type: String,
+  },
+  des5: {
+    type: String,
+  },
+  audio5: {
+    type: String,
+  },
+  des6: {
+    type: String,
+  },
+  audio6: {
+    type: String,
+  },
+  des7: {
+    type: String,
+  },
+  audio7: {
+    type: String,
+  },
+  des8: {
+    type: String,
+  },
+  audio8: {
+    type: String,
+  },
+  des9: {
+    type: String,
+  },
+  audio9: {
+    type: String,
+  },
+  des10: {
+    type: String,
+  },
+  audio10: {
+    type: String,
+  },
+  des11: {
+    type: String,
+  },
+  audio11: {
+    type: String,
+  },
+  des12: {
+    type: String,
+  },
+  audio12: {
+    type: String,
+  },
+  des13: {
+    type: String,
+  },
+  audio13: {
+    type: String,
+  },
+  des14: {
+    type: String,
+  },
+  audio14: {
+    type: String,
+  },
+  des15: {
+    type: String,
+  },
+  audio15: {
+    type: String,
+  },
+  des16: {
+    type: String,
+  },
+  audio16: {
+    type: String,
+  },
+  des17: {
+    type: String,
+  },
+  audio17: {
+    type: String,
+  },
+  des18: {
+    type: String,
+  },
+  audio18: {
+    type: String,
+  },
+  des19: {
+    type: String,
+  },
+  audio19: {
+    type: String,
+  },
+  des20: {
+    type: String,
+  },
+  audio20: {
+    type: String,
+  },
+  des21: {
+    type: String,
+  },
+  audio21: {
+    type: String,
+  },
+  des22: {
+    type: String,
+  },
+  audio22: {
+    type: String,
+  },
+  des23: {
+    type: String,
+  },
+  audio23: {
+    type: String,
+  },
+  des24: {
+    type: String,
+  },
+  audio24: {
+    type: String,
+  },
+  des25: {
+    type: String,
+  },
+  audio25: {
+    type: String,
+  },
+  des26: {
+    type: String,
+  },
+  audio26: {
+    type: String,
+  },
+  des27: {
+    type: String,
+  },
+  audio27: {
+    type: String,
+  },
+  des28: {
+    type: String,
+  },
+  audio28: {
+    type: String,
+  },
+  des29: {
+    type: String,
+  },
+  audio29: {
+    type: String,
+  },
+  des30: {
+    type: String,
+  },
+  audio30: {
+    type: String,
+  },
+  des31: {
+    type: String,
+  },
+  audio31: {
+    type: String,
+  },
+  des32: {
+    type: String,
+  },
+  audio32: {
+    type: String,
+  },
+  des33: {
+    type: String,
+  },
+  audio33: {
+    type: String,
+  },
+  des34: {
+    type: String,
+  },
+  audio34: {
+    type: String,
+  },
+  des35: {
+    type: String,
+  },
+  audio35: {
+    type: String,
+  },
+  des36: {
+    type: String,
+  },
+  audio36: {
+    type: String,
+  },
+  des37: {
+    type: String,
+  },
+  audio37: {
+    type: String,
+  },
+  des38: {
+    type: String,
+  },
+  audio38: {
+    type: String,
+  },
+  des39: {
+    type: String,
+  },
+  audio39: {
+    type: String,
+  },
+  des40: {
+    type: String,
+  },
+  audio40: {
+    type: String,
+  },
+  des41: {
+    type: String,
+  },
+  audio41: {
+    type: String,
+  },
+  des42: {
+    type: String,
+  },
+  audio42: {
+    type: String,
+  }
 });
 
 const Sroject = mongoose.model("Sroject", schema);
