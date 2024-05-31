92b2b2923fca99c4d05d00b62135cf04b4d42d81
girishjain01 <gngit2010@gmail.com>
2024-05-31 21:06:34 +0530

    hhh

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 // app.use("/sroject", srojectRoutes);
 
 const dbURI = process.env.MONGO_URI;
